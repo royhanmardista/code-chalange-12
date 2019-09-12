74a83cd4efe9995ae3317317f678425330451967
Royhan Mardista <royhanm23@gmail.com>
2019-09-12 20:08:37 +0700

    minor change

--- changeMe.js
+++ changeMe.js
@@ -1,4 +1,7 @@
 function changeMe(arr) {
+    if (arr.length == 0 ) {
+        console.log('""');
+    }
     for (let i=0; i<arr.length; i++) {     
         var objectNew = {};
         objectNew.firstName = arr[i][0];
@@ -7,14 +10,21 @@ function changeMe(arr) {
         if (arr[i][3] === undefined || arr[i][3] > 2019) {
             objectNew.age = 'Invalid Birth Year';
         } else {
-            objectNew.age = 2018-arr[i][3];
+            objectNew.age = 2019-arr[i][3];
         }           
     console.log(i+1 +". " +  arr[i][0] + " "+ arr[i][1] + " : ", objectNew);    
     }
 }
 
-var input =  [['Platinum', 'Fox', 'female', 1995], ['John', 'Doe', 'male', 2000]] ;
-changeMe(input);
-//Platinum Fox: { firstName: 'Platinum', lastName: 'Fox', gender: 'female', age: 23 }
-//John Doe: { firstName: 'John', lastName: 'Doe', gender: 'male', age: 18 }
+changeMe([['Christ', 'Evans', 'Male', 1982], ['Robert', 'Downey', 'Male']]); // 1. Christ Evans:
+// { firstName: 'Christ',
+//   lastName: 'Evans',
+//   gender: 'Male',
+//   age: 37 }
+// 2. Robert Downey:
+// { firstName: 'Robert',
+//   lastName: 'Downey',
+//   gender: 'Male',
+//   age: 'Invalid Birth Year' }
+changeMe([]); // ""
 
