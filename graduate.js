a869d0aad38d68bb735728e6a2291c70ff366a4b
Royhan Mardista <royhanm23@gmail.com>
2019-09-13 16:05:46 +0700

    -- final

--- graduate.js
+++ graduate.js
@@ -1,93 +1,92 @@
 function graduates (students) {
-  // Code disini
-  var output = {};    
-  for (let j=0; j<students.length; j++){      
-      var keyName = students[j].class;         
-      if (!output[keyName]) {
-          if (students[j].score > 75) {
-              output[keyName]= [{name : students[j].name, score : students[j].score}]
-          } 
-      } else {
-          if (students[j].score > 75) {
-              output[keyName].push({name : students[j].name, score : students[j].score})
-          }
-      }
+    // Code disini
+    var output = {};    
+    for (let j=0; j<students.length; j++){      
+        var keyName = students[j].class;         
+        if (students[j].score > 75) {
+            if (!output[keyName]) {                
+                    output[keyName]= [{name : students[j].name, score : students[j].score}]
+                 
+            } else {                
+                    output[keyName].push({name : students[j].name, score : students[j].score})
+                }
+        }         
+    }
+    return output;
   }
-  return output;
-}
-console.log(graduates([
-    {
-      name: 'Dimitri',
-      score: 90,
-      class: 'foxes'
-    },
-    {
-      name: 'Alexei',
-      score: 85,
-      class: 'wolves'
-    },
-    {
-      name: 'Sergei',
-      score: 74,
-      class: 'foxes'
-    },
-    {
-      name: 'Anastasia',
-      score: 78,
-      class: 'wolves'
-    }
-  ]));
-  
-  // {
-  //   foxes: [
-  //     { name: 'Dimitri', score: 90 }
-  //   ],
-  //   wolves: [
-  //     { name: 'Alexei' , score: 85 },
-  //     { name: 'Anastasia', score: 78 }
-  //   ]
-  // }
-  
   console.log(graduates([
-    {
-      name: 'Alexander',
-      score: 100,
-      class: 'foxes'
-    },
-    {
-      name: 'Alisa',
-      score: 76,
-      class: 'wolves'
-    },
-    {
-      name: 'Vladimir',
-      score: 92,
-      class: 'foxes'
-    },
-    {
-      name: 'Albert',
-      score: 71,
-      class: 'wolves'
-    },
-    {
-      name: 'Viktor',
-      score: 80,
-      class: 'tigers'
-    }
-  ]));
-  
-  // {
-  //   foxes: [
-  //     { name: 'Alexander', score: 100 },
-  //     { name: 'Vladimir', score: 92 }
-  //   ],
-  //   wolves: [
-  //     { name: 'Alisa', score: 76 },
-  //   ],
-  //   tigers: [
-  //     { name: 'Viktor', score: 80 }
-  //   ]
-  // }
-  
-  
-  console.log(graduates([])); //{}
+      {
+        name: 'Dimitri',
+        score: 90,
+        class: 'foxes'
+      },
+      {
+        name: 'Alexei',
+        score: 85,
+        class: 'wolves'
+      },
+      {
+        name: 'Sergei',
+        score: 74,
+        class: 'foxes'
+      },
+      {
+        name: 'Anastasia',
+        score: 78,
+        class: 'wolves'
+      }
+    ]));
+    
+    // {
+    //   foxes: [
+    //     { name: 'Dimitri', score: 90 }
+    //   ],
+    //   wolves: [
+    //     { name: 'Alexei' , score: 85 },
+    //     { name: 'Anastasia', score: 78 }
+    //   ]
+    // }
+    
+    console.log(graduates([
+      {
+        name: 'Alexander',
+        score: 100,
+        class: 'foxes'
+      },
+      {
+        name: 'Alisa',
+        score: 76,
+        class: 'wolves'
+      },
+      {
+        name: 'Vladimir',
+        score: 92,
+        class: 'foxes'
+      },
+      {
+        name: 'Albert',
+        score: 71,
+        class: 'wolves'
+      },
+      {
+        name: 'Viktor',
+        score: 80,
+        class: 'tigers'
+      }
+    ]));
+    
+    // {
+    //   foxes: [
+    //     { name: 'Alexander', score: 100 },
+    //     { name: 'Vladimir', score: 92 }
+    //   ],
+    //   wolves: [
+    //     { name: 'Alisa', score: 76 },
+    //   ],
+    //   tigers: [
+    //     { name: 'Viktor', score: 80 }
+    //   ]
+    // }
+    
+    
+    console.log(graduates([])); //{}
